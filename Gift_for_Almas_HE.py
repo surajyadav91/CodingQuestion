12aaf9544b7114084399aa1c74bb89678f37d538
surajyadav91 <86972819+surajyadav91@users.noreply.github.com>
2021-09-20 05:30:05 +0530

    Create Gift_for_Almas_HE.py

---
+++ Gift_for_Almas_HE.py
@@ -0,0 +1,22 @@
+
+n=int(input())
+array=[]
+for i in range(n):
+    tmp=list(map(int,input().split()))
+    array.append(tmp)
+
+string=input()
+
+import numpy as np 
+
+n_arr=np.array(array)
+
+for i in string:
+    if i == 'L':
+        #left 90 degree / counter clock wise / axis 0 to 1
+        n_arr=np.rot90(n_arr,1,(0,1))
+    else:
+        n_arr=np.rot90(n_arr,1,(1,0))
+
+for i in n_arr:
+    print(*i)
