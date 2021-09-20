b445644e55bc46992c8cb533a1ac11d4eb372366
surajyadav91 <86972819+surajyadav91@users.noreply.github.com>
2021-09-20 06:07:09 +0530

    Update Gift_for_Almas_HE.py

--- Gift_for_Almas_HE.py
+++ Gift_for_Almas_HE.py
@@ -1,5 +1,6 @@
 #Problem Link : https://www.hackerearth.com/practice/data-structures/arrays/multi-dimensional/practice-problems/algorithm/gift-for-almas-3-33d2f7c7/
 
+
 n=int(input())
 array=[]
 for i in range(n):
@@ -12,12 +13,14 @@ import numpy as np
 
 n_arr=np.array(array)
 
-for i in string:
-    if i == 'L':
-        #left 90 degree / counter clock wise / axis 0 to 1
-        n_arr=np.rot90(n_arr,1,(0,1))
-    else:
-        n_arr=np.rot90(n_arr,1,(1,0))
+
+#only 3 rotations , will check if we cancel LR or RL rotation
+value = sum([1 if i == 'L' else -1  for i in string])
+if value in [-3,1]:
+    #left 90 degree / counter clock wise / axis 0 to 1
+    n_arr=np.rot90(n_arr,1,(0,1))
+else:
+    n_arr=np.rot90(n_arr,1,(1,0))
 
 for i in n_arr:
     print(*i)
